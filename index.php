08c8e0525592bba24d2fce3b2889eaebc1518e06
BrittyB <BenjaminBritcliffe@outlook.com>
2017-04-30 16:37:35 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<html>
+<head>
+<title>Login Form</title>
+<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div class="container">
+<img src="images/man.png"/>
+<form>
+<div class="form-input">
+	<input type="text" name="username" placeholder=" Enter Username">
+</div>
+<div class="form-input">
+	<input type="password" name="password" placeholder="Enter Password">
+</div>
+<input type="submit" name="submit" value="login" class="btn-login">
+</form><br/>
+<a href="#">Forgot Password?</a>
+</div>
+<footer>
+<p>Login Form Made By <a class="footer-text" href="#">Benjamin Britcliffe</a></p></footer>
+
+</body>
+</html>
